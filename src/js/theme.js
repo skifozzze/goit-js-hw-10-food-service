ef3b625679800de84a3a255f7099c2e1ec317d47
skifozzze <skifozzze@gmail.com>
2023-03-23 19:00:52 +0200

    fin

--- src/js/theme.js
+++ src/js/theme.js
@@ -4,8 +4,28 @@ const Theme = {
 };
 
 const switchEl = document.querySelector('#theme-switch-toggle');
+const bodyEl = document.querySelector('body');
+
 switchEl.addEventListener('change', onCheckboxChange);
 
+onDarkThemeReset();
+
 function onCheckboxChange(evt) {
-  console.log(evt.tagret.value);
+  if (this.checked) {
+    bodyEl.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyEl.classList.remove(Theme.DARK);
+    bodyEl.classList.add(Theme.LIGHT);
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function onDarkThemeReset() {
+  const checkedTheme = localStorage.getItem('theme');
+  if (checkedTheme === Theme.DARK) {
+    switchEl.checked = true;
+    bodyEl.classList.add(Theme.DARK);
+  }
 }
